25628397205a7a098cf66137fc248450b32354ed
zhangyilong <yilongzhang@sohu-inc.com>
2021-05-30 15:03:37 +0800

    localsticker init

---
+++ app/src/main/java/com/henryford/sticker/AboutActivity.java
@@ -0,0 +1,45 @@
+package com.henryford.sticker;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        FrameLayout twitter=findViewById(R.id.tweet);
+        FrameLayout play=findViewById(R.id.googleplay);
+        twitter.setOnClickListener(v -> openWeb("https://twitter.com/CompanyAbsolute"));
+        play.setOnClickListener(v ->
+            openStore());
+    }
+
+    private void openWeb(String website) {
+        Uri uri = Uri.parse(website);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+    private void openStore(){
+        Uri uri = Uri.parse("market://details?id=" + getApplicationContext().getPackageName());
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+        // To count with Play market backstack, After pressing back button,
+        // to taken back to our application, we need to add following flags to intent.
+        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        try {
+            startActivity(goToMarket);
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=" + getApplicationContext().getPackageName())));
+        }
+    }
+
+}
